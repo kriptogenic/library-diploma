4a45a9f46f838b9a46441bdd4dbb16568c6bb687
Kriptogenic <kripton253@gmail.com>
2024-04-21 12:33:22 +0500

    route ref

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,15 @@ use App\Http\Controllers\VisitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('books', [BookController::class, 'index']);
-
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::get('books', BookController::class);
     Route::post('visit', VisitController::class);
 });
