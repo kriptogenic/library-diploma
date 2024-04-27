6ec4a729c413abce532187d7702bad81d9bdac09
Kriptogenic <kripton253@gmail.com>
2024-04-27 22:04:25 +0500

    poster url

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
     public function __invoke()
     {
-        return Book::paginate();
+        return Book::paginate()->map(function (Book $book) {
+            return [
+                ...$book->toArray(),
+                'poster' => Storage::url($book->poster)
+            ];
+        });
     }
 }
